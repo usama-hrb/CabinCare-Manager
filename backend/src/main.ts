50fd00ea7d53eb90a4a981101a95ebbc8ebed7c1
Usama <usama@example.com>
2025-12-08 23:25:17 +0100

    Add Swagger API Documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const swagger = new DocumentBuilder()
+    .setTitle('CabinCare Manager - App API')
+    .setVersion('1.0')
+    .build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swagger);
+  //http://localhost:3000/swagger
+  SwaggerModule.setup('swagger', app, swaggerDoc);
 
   await app.listen(process.env.PORT ?? 3000);
 }
